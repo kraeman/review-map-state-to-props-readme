14facd5e8cc125024b60bc6b309323d7168a7591
Krae Wind <kraewind@gmail.com>
2021-07-15 13:43:20 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
@@ -6,19 +7,19 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.dispatch({
+    this.props.store.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.dispatch({
+    this.props.store.dispatch({
       type: 'GET_COUNT_OF_USERS',
-    });
+    })
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -27,14 +28,14 @@ class App extends Component {
           <button onClick={() => this.handleOnClickUsers()}>
             Click to change user count
           </button>
-          <p>{this.props.items.length}</p>
+          <p> {this.props.items.length}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
